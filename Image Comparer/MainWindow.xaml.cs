c646c6731918a77e231b89340b0a40e6472ff34d
roman-minyaylov <roman.minyaylov@gmail.com>
2019-06-29 15:54:51 +0300

    Update to SDK version 11.1.40

--- Image Comparer/MainWindow.xaml.cs
+++ Image Comparer/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ImageComparer.Properties;
-using VisioForge.MediaFramework;
+
 using VisioForge.VideoFingerPrinting;
 
 namespace ImageComparer
